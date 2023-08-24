e3a9fe7f149f47864cb4653edd91a685325dea89
Iman Kianian <42094881+iman2693@users.noreply.github.com>
2023-08-24 04:55:14 +0330

    Update satsolver.py

--- satsolver.py
+++ satsolver.py
@@ -1,99 +1,89 @@
 import string
 import itertools
-print("""
-And  ^ , ∧
-Or  v, ∨
-Not ~ , ¬
-Then  >
-IFF <>
-All Variables have to be 1 Char long
-""")
 
-expression = input("please Enter A Currect Expression : ")
-expression=expression.replace(' ','')
-expression=expression.replace('v','∨')
-expression=expression.replace('^','∧')
-expression=expression.replace('~','¬')
-correctvariablelist=list(string.ascii_lowercase)
-correctvariablelist.remove('v')
-variablelist=[]
-Interpretations=[]
-for i in range(len(expression)):
-    if(expression[i] in correctvariablelist and expression[i] not in variablelist):
-        variablelist.append(expression[i])
-Interpretations=[]
-for roll in itertools.product([0,1], repeat = len(variablelist)):
-    Interpretations.append(roll)
+# Print logical symbols and their ASCII characters.
+print("And  ^ , ∧\nOr  v, ∨\nNot ~ , ¬\nThen  >\nIFF <>\nAll variables must be 1 character long\n")
 
+# Get and preprocess the input expression.
+expression = input("Please enter a logical expression: ").replace(' ', '').replace('v', '∨').replace('^', '∧').replace('~', '¬')
 
-flag=False
-for i in range(len(Interpretations)):
+# Define correct variable list using lowercase letters (excluding 'v').
+correct_variable_list = list(string.ascii_lowercase)
+correct_variable_list.remove('v')
+
+# Extract unique variables from the expression.
+variable_list = [char for char in expression if char in correct_variable_list]
+
+# Generate all possible truth assignments for the variables.
+interpretations = list(itertools.product([0, 1], repeat=len(variable_list)))
+
+# Initialize a flag to track satisfiability.
+flag = False
+
+# Iterate through each interpretation to check satisfiability.
+for i, interpretation in enumerate(interpretations):
     exp = expression
-    for j in range(len(variablelist)):
-        var=variablelist[j]
-        exp=exp.replace(var,str(Interpretations[i][j]))
+    
+    # Substitute truth values into the expression.
+    for var, value in zip(variable_list, interpretation):
+        exp = exp.replace(var, str(value))
 
+    # Define simplification rules for replacements.
+    simplifications = {
+        'F': '0',
+        'T': '1',
+        '¬(1)': '(0)',
+        '¬(0)': '(1)',
+        '¬1': '0',
+        '¬0': '1',
+        '(1∨0)': '(1)',
+        '(0∨1)': '(1)',
+        '(1∨1)': '(1)',
+        '(0∨0)': '(0)',
+        '1∨0': '1',
+        '0∨1': '1',
+        '1∨1': '1',
+        '0∨0': '0',
+        '(1∧1)': '(1)',
+        '(1∧0)': '(0)',
+        '(0∧1)': '(0)',
+        '(0∧0)': '(0)',
+        '1∧1': '1',
+        '1∧0': '0',
+        '0∧1': '0',
+        '0∧0': '0',
+        '(1>1)': '(1)',
+        '(0>1)': '(1)',
+        '(0>0)': '(1)',
+        '(1>0)': '(0)',
+        '1>1': '1',
+        '0>1': '1',
+        '0>0': '1',
+        '1>0': '0',
+        '(1<>1)': '(1)',
+        '(0<>0)': '(1)',
+        '(1<>0)': '(0)',
+        '(0<>1)': '(0)',
+        '1<>1': '1',
+        '0<>0': '1',
+        '1<>0': '0',
+        '0<>1': '0',
+        '(1)': '1',
+        '(0)': '0'
+    }
+
+    # Apply simplifications iteratively until expression is simplified.
     while exp != '0' and exp != '1':
-        # for T=True or F=False  ... so Your Variable cannot be T or F
-        exp = exp.replace('F', '0')
-        exp = exp.replace('T', '1')
-        # for ¬
-        exp = exp.replace('¬(1)', '(0)')
-        exp = exp.replace('¬(0)', '(1)')
+        for key, value in simplifications.items():
+            exp = exp.replace(key, value)
 
-        while '¬1' in exp or '¬0' in exp:
-            exp = exp.replace('¬1', '0')
-            exp = exp.replace('¬0', '1')
-        # for ∨
-        exp = exp.replace('(1∨0)', '(1)')
-        exp = exp.replace('(0∨1)', '(1)')
-        exp = exp.replace('(1∨1)', '(1)')
-        exp = exp.replace('(0∨0)', '(0)')
+    # Check if the expression is satisfied.
+    if exp == '1':
+        print('This Expression is Satisfiable for:')
+        for var, value in zip(variable_list, interpretation):
+            print(var, ":", value)
+        flag = True
 
-        # for ∧
-        exp = exp.replace('(1∧1)', '(1)')
-        exp = exp.replace('(1∧0)', '(0)')
-        exp = exp.replace('(0∧1)', '(0)')
-        exp = exp.replace('(0∧0)', '(0)')
-
-        # for >
-        exp = exp.replace('(1>1)', '(1)')
-        exp = exp.replace('(0>1)', '(1)')
-        exp = exp.replace('(0>0)', '(1)')
-        exp = exp.replace('(1>0)', '(0)')
-
-        # for <>
-        exp = exp.replace('(1<>1)', '(1)')
-        exp = exp.replace('(0<>0)', '(1)')
-        exp = exp.replace('(1<>0)', '(0)')
-        exp = exp.replace('(0<>1)', '(0)')
-
-        # Others
-        exp = exp.replace('1∨0', '1')
-        exp = exp.replace('0∨1', '1')
-        exp = exp.replace('1∨1', '1')
-        exp = exp.replace('0∨0', '0')
-        exp = exp.replace('1∧1', '1')
-        exp = exp.replace('1∧0', '0')
-        exp = exp.replace('0∧1', '0')
-        exp = exp.replace('0∧0', '0')
-        exp = exp.replace('1>1', '1')
-        exp = exp.replace('0>1', '1')
-        exp = exp.replace('0>0', '1')
-        exp = exp.replace('1>0', '0')
-        exp = exp.replace('1<>1', '1')
-        exp = exp.replace('0<>0', '1')
-        exp = exp.replace('1<>0', '0')
-        exp = exp.replace('0<>1', '0')
-
-        exp = exp.replace('(1)', '1')
-        exp = exp.replace('(0)', '0')
-
-    if exp=='1':
-        print('This Expression is Satisfiable for : ')
-        for k in range(len(variablelist)):
-            print(variablelist[k]," : ",Interpretations[i][k])
-        flag=True
-
-if flag==False:
+# If no satisfying interpretation is found, print a message.
+if not flag:
     print('This Expression is Not Satisfiable')
